830931054f3c3b65821d9665c9b36383fd7deb7c
Vladimir Petko <vladimir.petko@canonical.com>
2025-07-31 16:42:47 +1200

    feat: gradle refactoring

---
+++ src/main/java/org/debian/gradle/rewrite/Main.java
@@ -0,0 +1,139 @@
+package org.debian.gradle.rewrite;
+
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.Parser;
+import org.openrewrite.Recipe;
+import org.openrewrite.RecipeRun;
+import org.openrewrite.Result;
+import org.openrewrite.SourceFile;
+import org.openrewrite.gradle.GradleParser;
+import org.openrewrite.groovy.GroovyParser;
+import org.openrewrite.internal.InMemoryLargeSourceSet;
+import org.openrewrite.kotlin.KotlinParser;
+import org.openrewrite.tree.ParseError;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+import java.util.function.Consumer;
+
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length < 2) {
+            throw new IllegalArgumentException("<refactoring>.yaml baseDir");
+        }
+        InMemoryExecutionContext context = new InMemoryExecutionContext(new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) {
+
+                logger.error(throwable.getMessage(), throwable);
+            }
+        });
+
+        Yaml yaml = new Yaml();
+        Map<String, Object> config = yaml.load(new FileReader(args[0]));
+        var baseDir = Path.of(args[1]);
+        var sources = parseGradle(baseDir, context);
+        boolean kotlinDsl = sources.stream()
+                .filter(x -> x.getSourcePath().toString().equals("build.gradle"))
+                .findAny()
+                .isEmpty();
+
+        for (var source : sources) {
+            if (source instanceof ParseError) {
+                System.out.println("Failed to parse: " + source.getSourcePath());
+            }
+        }
+        for (var recipe : config.keySet()) {
+            Map<String, Object> recipeData = (Map<String, Object> ) config.get(recipe);
+            Recipe recipeImpl = createRecipe(kotlinDsl, recipeData);
+            var files = (List<String>)recipeData.get("files");
+            if (files == null) {
+                files = List.of();
+            }
+            final HashSet<String> fileSet = new HashSet<>(files);
+            applyRecipe( baseDir,
+                    recipeImpl,
+                    sources.stream().filter( x -> fileSet.isEmpty() || fileSet.contains(x.getSourcePath().toString())).toList(),
+                    context);
+        }
+        System.out.println("Processed " + sources.size());
+    }
+
+    private static Recipe createRecipe(boolean kotlinDsl, Map<String, Object> recipeData) {
+        String recipe = String.valueOf(recipeData.get("recipe"));
+        switch (recipe) {
+            case "remove" : {
+                var removeMethods = (List<String>)recipeData.get("remove-methods");
+                if (removeMethods == null) {
+                    removeMethods = List.of();
+                }
+                var removePlugins = (List<String>)recipeData.get("remove-plugins");
+                if (removePlugins == null) {
+                    removePlugins = List.of();
+                }
+                return new RemoveExtensionRecipe(kotlinDsl, removePlugins, removeMethods);
+            }
+        }
+        throw new IllegalArgumentException("Unknown recipe "+ recipe);
+    }
+
+    public static boolean applyRecipe(Path baseDir, Recipe r, List<SourceFile> sourceFiles, ExecutionContext context)
+            throws IOException {
+        RecipeRun run = r.run(new InMemoryLargeSourceSet(sourceFiles), context);
+        List<Result> results = run.getChangeset().getAllResults();
+        for (Result result : results) {
+            SourceFile after = result.getAfter();
+            Files.writeString(baseDir.resolve(after.getSourcePath()), result.getAfter().printAll());
+        }
+        return !results.isEmpty();
+    }
+
+    private static List<SourceFile> parseGradle(Path baseDir, InMemoryExecutionContext context) throws IOException {
+        Parser.Builder builder = GradleParser.builder()
+                .groovyParser(GroovyParser.builder().logCompilationWarningsAndErrors(true))
+                .kotlinParser(KotlinParser.builder().
+                        logCompilationWarningsAndErrors(true)
+                        .isKotlinScript(true)
+                        .languageLevel(KotlinParser.KotlinLanguageLevel.KOTLIN_1_9)
+                );
+
+        Parser p = builder.build();
+        final HashSet<String> gradleNames = new HashSet<>(Arrays.asList("build.gradle", "build.gradle.kts",
+                "settings.gradle", "settings.gradle.kts", "init.gradle", "init.gradle.kts"));
+        ArrayList<Path> files = new ArrayList<>();
+        Stack<Path> toDo = new Stack<>();
+        toDo.push(baseDir);
+        while (!toDo.isEmpty()) {
+            Path top = toDo.pop();
+            Files.list(top).forEach(path -> {
+                if (Files.isRegularFile(path) && (path.toString().endsWith(".gradle")
+                        || path.toString().endsWith(".gradle.kts")
+                        || gradleNames.contains(path.toFile().getName()))) {
+                    files.add(path);
+                } else if (Files.isDirectory(path)) {
+                    toDo.push(path);
+                }
+            });
+        }
+        //files.clear();
+        //files.add(Path.of(baseDir + "/build.gradle.kts"));
+        return p.parse(files, baseDir, context).toList();
+    }
+
+}
---
+++ src/main/java/org/debian/gradle/rewrite/RemoveExtensionRecipe.java
@@ -0,0 +1,167 @@
+package org.debian.gradle.rewrite;
+
+import org.gradle.workers.internal.AbstractClassLoaderWorker;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.NlsRewrite;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.groovy.GroovyIsoVisitor;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JRightPadded;
+import org.openrewrite.java.tree.Statement;
+import org.openrewrite.kotlin.KotlinIsoVisitor;
+import org.openrewrite.kotlin.tree.K;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.openrewrite.Tree.randomId;
+
+public class RemoveExtensionRecipe extends Recipe {
+
+    private static final String PLUGIN_BLOCK = "PLUGIN_BLOCK";
+
+    private final boolean kotlinDsl;
+    private final HashSet<String> removeMethods;
+    private final HashSet<String> removePlugins;
+
+    public RemoveExtensionRecipe(boolean kotlinDsl, List<String> removePlugins, List<String> removeMethods) {
+        this.kotlinDsl = kotlinDsl;
+        this.removePlugins = new HashSet<>(removePlugins);
+        this.removeMethods = new HashSet<>(removeMethods);
+    }
+
+    @Override
+    public @NlsRewrite.DisplayName String getDisplayName() {
+        return "Remove unsupported extensions";
+    }
+
+    @Override
+    public @NlsRewrite.Description String getDescription() {
+        return "Removes unsupported extensions from the gradle build.";
+    }
+
+    /**
+     * if ("enableFeaturePreview".equals(method.getSimpleName()) &&
+     * method.getArguments().size() == 1 &&
+     * J.Literal.isLiteralValue(method.getArguments().get(0), previewFeatureName)) {
+     * return null;
+     * }
+     * return method;
+     *
+     * @return
+     */
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        TreeVisitor<?, ExecutionContext> ret;
+        if (kotlinDsl) {
+            return new KotlinIsoVisitor<ExecutionContext>() {
+
+                @Override
+                public J.Lambda visitLambda(J.Lambda lambda, ExecutionContext executionContext) {
+                    J.Lambda newLambda = filterLambda(lambda, executionContext);
+                    if (newLambda != lambda) {
+                        return newLambda;
+                    }
+                    return super.visitLambda(lambda, executionContext);
+                }
+
+                @Override
+                public J visitAnnotatedExpression(K.AnnotatedExpression annotatedExpression, ExecutionContext executionContext) {
+                    if (annotatedExpression.getExpression() instanceof J.MethodInvocation) {
+                        J.MethodInvocation m = visitMethodInvocation((J.MethodInvocation) annotatedExpression.getExpression(), executionContext);
+                        if (m == null) {
+                            return null;
+                        }
+                        annotatedExpression =  annotatedExpression.withExpression(m);
+                    }
+                    return super.visitAnnotatedExpression(annotatedExpression, executionContext);
+                }
+
+                @Override
+                public J.@Nullable MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+                    J.MethodInvocation m = filterMethodInvocation(method, ctx);
+                    if (m == null) {
+                        return null;
+                    }
+                    return super.visitMethodInvocation(m, ctx);
+                }
+
+                @Override
+                public @Nullable J postVisit(@NonNull J tree, ExecutionContext executionContext) {
+                    if (tree instanceof J.MethodInvocation) {
+                        J.MethodInvocation call = (J.MethodInvocation) tree;
+                        if (call.getSimpleName().equals("plugins")) {
+                            executionContext.pollMessage(PLUGIN_BLOCK);
+                        }
+                    }
+                    return super.postVisit(tree, executionContext);
+                }
+            };
+        }
+        return new GroovyIsoVisitor<ExecutionContext>() {
+
+            @Override
+            public J.@Nullable MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+                J.MethodInvocation m = filterMethodInvocation(method, ctx);
+                if (m == null) {
+                    return null;
+                }
+                return super.visitMethodInvocation(m, ctx);
+            }
+        };
+    }
+
+    private J.Lambda filterLambda(J.Lambda lambda, ExecutionContext executionContext) {
+        if (executionContext.getMessage(PLUGIN_BLOCK, false)) {
+            J.Block block = (J.Block) lambda.getBody();
+            List<Statement> stm = block.getStatements();
+            List<Statement> newStm = stm.stream().filter( x -> {
+                if (x instanceof K.ExpressionStatement) {
+                    K.ExpressionStatement kStm = (K.ExpressionStatement)x;
+                    //return !"signing".equals(kStm.getExpression().toString());
+                }
+                return true;
+            }).toList();
+            if (newStm.size() == stm.size()) {
+                return lambda;
+            }
+            return lambda.withBody(new J.Block(randomId(), block.getPrefix(), block.getMarkers(),
+                    JRightPadded.build(block.isStatic()),
+                    newStm.stream().map(JRightPadded::build).toList(), block.getEnd()));
+        }
+        return lambda;
+    }
+
+    private J.@Nullable MethodInvocation filterMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+
+        if (removeMethods.contains(method.getSimpleName())) {
+            return null;
+        }
+
+        if ("plugins".equals(method.getSimpleName())) {
+            ctx.putMessage(PLUGIN_BLOCK, true);
+        }
+
+        if ("id".equals(method.getSimpleName()) || "alias".equals(method.getSimpleName())) {
+            String id = method.getArguments().get(0).toString();
+            if (removePlugins.contains(id)) {
+                return null;
+            }
+        }
+        if ("version".equals(method.getSimpleName())) {
+            J.MethodInvocation call = (J.MethodInvocation) method.getSelect();
+            if (call == null) {
+                return method;
+            }
+            String id = call.getArguments().get(0).toString();
+            if (removePlugins.contains(id)) {
+                return null;
+            }
+        }
+        return method;
+    }
+}
